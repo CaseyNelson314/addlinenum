02f3993ce5d59104762171b4f2c95fdcd79c2c64
Okawa Yusuke <caseynelson314@gmail.com>
2025-01-23 18:44:22 +0900

    web 番に変更

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useRef, useState } from "react";
+import { Editor } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
+import "./App.css";
+
+const defaultSource = `#include <iostream>
+
+int main()
+{
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}`;
+
+function App() {
+  const distRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+
+  const [, setText] = useState<string | undefined>("");
+
+  const addLineNumber = (raw: string) => {
+    const lines = raw.split("\n");
+    const nDigits = lines.length.toString().length; // 桁数
+    return lines
+      .map((line, i) => {
+        return (i + 1).toString().padStart(nDigits, " ") + ":  " + line;
+      })
+      .join("\n");
+  };
+
+  const onSourceChange = (t: string | undefined) => {
+    setText(t);
+    if (distRef.current) {
+      const model = distRef.current.getModel();
+      model?.setValue(addLineNumber(t || ""));
+    }
+  };
+
+  return (
+    <>
+      <div className="flex bg-gray-900 text-white">
+        <div className="flex-1">
+          <Editor
+            height="100vh"
+            defaultLanguage="cpp"
+            defaultValue={defaultSource}
+            theme="vs-dark"
+            options={{
+              lineNumbers: "off",
+              minimap: { enabled: false },
+              folding: false,
+              tabSize: 4,
+            }}
+            onChange={onSourceChange}
+          />
+        </div>
+        <div className="flex-1">
+          <Editor
+            height="100vh"
+            defaultLanguage="cpp"
+            defaultValue=""
+            theme="vs-dark"
+            options={{
+              readOnly: true,
+              lineNumbers: "off",
+              minimap: { enabled: false },
+              folding: false,
+            }}
+            onMount={(editor) => {
+              distRef.current = editor;
+              onSourceChange(defaultSource);
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
